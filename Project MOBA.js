970d6dfee5e6b6f33ab0d574195b170448a6cd64
Mateus Zamorano <52588123+lorde6999@users.noreply.github.com>
2024-05-04 12:31:24 -0300

    Update Project MOBA.js
    Project MOBA - O início;
    - Alguns Personagens
    - Pontos de Liga (PDL)
    - Marcas Ranqueadas (Bronze, Prata, Ouro...)

---
+++ Project MOBA.js
@@ -0,0 +1,13 @@
+const message = "Legenda: PDL = Pontos de Liga"
+console.log(message);
+let heroi = ["SH4NK","Kankur", "Xanner", "Skax", "Glory", "Lautex", "Bromance", "Drakante"];
+const experiencia = [" Ferro - PDL - 567", " Bronze - PDL - 1357", " Prata - PDL - 2541", " Ouro - PDL - 6897", " Platina - PDL - 7999", " Ascentente - PDL - 8313", " Imortal - PDL - 9631", " Radiante - PDL - 12478"];
+const nickNames = ["Shank","Kankur", "Xanner", "Skax", "Glory", "Lautex", "Bromance", "Drakante"];
+console.log (heroi[0] + experiencia[0])
+console.log (heroi[1] + experiencia[1])
+console.log (heroi[2] + experiencia[2])
+console.log (heroi[3] + experiencia[3])
+console.log (heroi[4] + experiencia[4])
+console.log (heroi[5] + experiencia[5])
+console.log (heroi[6] + experiencia[6])
+console.log (heroi[7] + experiencia[7])
